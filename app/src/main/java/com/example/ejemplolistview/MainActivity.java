0cb58e65ffb41613bc275f0fc83c0b0e57fbd9f4
Dani-Asus <danielsosa1111@hotmail.com>
2020-09-16 19:30:19 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ejemplolistview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ejemplolistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Inmueble> lista=new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cargarDatos();
+        generarListView();
+    }
+
+    public void generarListView(){
+
+        ArrayAdapter<Inmueble> adapter=new ListaAdapter(this,R.layout.item,lista,getLayoutInflater());
+        ListView lv=findViewById(R.id.miLista);
+        lv.setAdapter(adapter);
+    }
+
+    public void cargarDatos(){
+
+        lista.add(new Inmueble(R.drawable.caballo,"Caballo", 10000));
+        lista.add(new Inmueble(R.drawable.minion,"Minion", 500000));
+        lista.add(new Inmueble(R.drawable.sapo,"Sapo", 6500000));
+    }
+
+}
